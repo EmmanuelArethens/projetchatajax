1437b3739c9bb5bb92f0542ad867e083dce8a852
EmmanuelArethens <emmanuelarethens1@gmail.com>
2017-08-04 16:06:18 +0200

    test

--- dof.php
+++ dof.php
@@ -1,5 +1,12 @@
 <?php
-if(isset($_POST["chibre"])){
-    echo('Asuka : '.$_POST["chibre"]);}
+include_once './Database.php';
+include_once './Messages.php';
 
+
+$db = new DataChat();
+    echo($_POST["textare"]);
+
+    $date = date("Y-m-d H:i:s");
+    $post = new Messages($_POST['textare'], $date);
+    $db->createMessage($post);
 ?>
